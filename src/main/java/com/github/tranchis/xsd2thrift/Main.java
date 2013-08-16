d82837c9b38572a24981d6eac47162dab6a6dad7
nagkumar <nagendra.raja@tejasoft.com>
2013-08-16 20:10:23 +0530

    option for file name is not --output it is --filename
    Changed the help accordingly

--- src/main/java/com/github/tranchis/xsd2thrift/Main.java
+++ src/main/java/com/github/tranchis/xsd2thrift/Main.java
@@ -35,12 +35,12 @@ public class Main
 {
 	private static boolean	correct;
 	private static String	usage = "" + 
-			"Usage: java xsd2thrift.jar [--thrift] [--protobuf] [--output=FILENAME]\n" +
+			"Usage: java xsd2thrift.jar [--thrift] [--protobuf] [--filename=FILENAME]\n" +
 			"                           [--package=NAME] filename.xsd\n" + 
 			"\n" + 
 			"  --thrift          		: convert to Thrift\n" + 
 			"  --protobuf        		: convert to Protocol Buffers\n" + 
-			"  --output=FILENAME 		: store the result in FILENAME instead of standard output\n" + 
+			"  --filename=FILENAME 		: store the result in FILENAME instead of standard output\n" + 
 			"  --package=NAME    		: set namespace/package of the output file\n" + 
 			"  --nestEnums=true|false	: nest enum declaration within messages that reference them, only supported by protobuf, defaults to true\n" + 
 			"";
